9675bdc9942fdbf5d2dc3f6890c5f7d62f197cc6
charlieabhishek <abcptech@gmail.com>
2019-03-11 23:51:35 +0900

    Making data task testable.

---
+++ UnitTestingDemo/UnitTestingDemo/MockURLSessionDataTask.swift
@@ -0,0 +1,9 @@
+//
+//  MockURLSessionDataTask.swift
+//  UnitTestingDemo
+//
+//  Created by Abhishek Kumar on 11/03/19.
+//  Copyright © 2019 CharlieAbhishek. All rights reserved.
+//
+
+import Foundation
